772094fd61edd21e33c312eeef5edf50da8bc397
Xasiri <asiri1234@gmail.com>
2020-08-13 10:25:42 +0530

    initial commit

---
+++ src/store/actions/auth.js
@@ -0,0 +1,94 @@
+//setup authentication related apps
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        idtoken: token,
+        userId: userId
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+export const logout =() =>{
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
+    return{
+        
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+export const checkAuthTimeout=(expirationTime) =>{
+    return dispatch=>{
+        setTimeout(()=>{
+            dispatch(logout());
+        },expirationTime*1000)
+    }
+}
+
+export const auth = (email, password, isSignUp) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData={
+            email: email,
+            password: password,
+            returnSecureToken:true
+        };
+        let url ='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC58nv8Dp9X8-x2axx7dpwTeMS-AJlBpm0';
+        if(!isSignUp){
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC58nv8Dp9X8-x2axx7dpwTeMS-AJlBpm0';
+        }
+        axios.post(url, authData)
+            .then(response =>{
+                console.log(response);
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn*1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
+            })
+            .catch(err =>{
+                console.log(err);
+                dispatch(authFail(err.response.data.error));
+            });
+    };
+};
+
+export const setAuthRedirectPath =(path) =>{
+    return{
+        type:actionTypes.SET_AUTH_REDIRECT_PATH,
+        path:path
+    };
+};
+export const authCheckState =()=>{
+    return dispatch=>{
+        const token = localStorage.getItem('token');
+        if(!token){
+            dispatch(logout());
+        }else{
+            const expirationDate = localStorage.getItem('expirationDate');
+            if(expirationDate >new Date()){
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout(expirationDate.getSeconds()- new Date().getSeconds()));
+            } else{
+                dispatch(logout());
+            }
+        }
+
+    }
+}
